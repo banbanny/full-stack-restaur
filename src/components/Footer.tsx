21a5fb209e385fe0d8675d829b714e9749e48c43
shindihi <ivannemarie.obediente@hcdc.edu.ph>
2024-10-24 00:29:07 +0800

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl">KUYA CARD&apos;S</Link>
+      <Link href="/" className="font-bold text-xl">KUYA CARDS</Link>
       <p>© ALL RIGHTS RESERVED.</p>
     </div>
   );
