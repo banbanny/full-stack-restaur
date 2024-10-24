128e5acc89e685686b35b8a6cbad6ff1ee17c383
shindihi <ivannemarie.obediente@hcdc.edu.ph>
2024-10-24 19:46:49 +0800

    fix commit

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,22 +1,18 @@
 import { prisma } from "@/utils/connect";
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const PUT = async (req: NextRequest, { params }: { params: { intentId: string } }) => {
-  const { intentId } = params;
-
-  console.log("Received intentId:", intentId); // Debugging log
+  console.log("Received intentId:", params.intentId); // Add logs to debug
 
   try {
     const updatedOrder = await prisma.order.update({
-      where: { intent_id: intentId },
+      where: { intent_id: params.intentId },
       data: { status: "Being prepared!" },
     });
-    
-    console.log("Order update result:", updatedOrder); // Log result of the update
-
+    console.log("Order updated successfully:", updatedOrder); // Add log for success
     return NextResponse.json({ message: "Order has been updated" }, { status: 200 });
   } catch (err) {
-    console.log("Error during update:", err); // Detailed error log
+    console.log("Error occurred during Prisma query:", err); // Log error
     return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
   }
 };
